56d46b827a7bd1335d1f627335ff42583aab1149
Kartik Dhomne <kartik.dhomne@acceleratebs.com>
2023-12-03 17:39:55 +0530

    changes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import Body from "./components/Body";
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-500">
-hey
+    <div className="App">
+      <Body />
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+      <img
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/c906271d-7184-4eec-83c9-b6d4c1a068ec/728874a6-eeda-400a-9bcf-a935a1408a4f/IN-en-20231127-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+        alt="logo"
+      />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
